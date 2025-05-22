28d7ad028a19f088a9cf538771ff8108e53e0d52
brodybigwood <brodybigwood@gmail.com>
2025-05-21 23:56:21 -0700

    only let admins onto unfinished pages

--- php/content.php
+++ php/content.php
@@ -19,9 +19,37 @@ if (!in_array($page, $allowedTypes)) {
     exit();
 }
 
+$username = $_SESSION['username'];
+$isAdmin = false;
+
+try {
+    $db = new SQLite3('../weeks.db');
+    $stmt = $db->prepare("SELECT is_admin FROM Employees WHERE name = :username LIMIT 1");
+    $stmt->bindValue(':username',$username, SQLITE3_TEXT);
+
+    $result = $stmt->execute();
+
+    if($result) {
+        $row = $result->fetchArray(SQLITE3_ASSOC);
+
+        if($row) {
+            if($row['is_admin'] ==1) {
+                $isAdmin = true;
+            }
+        }
+        $result->finalize();
+    }
+    $db->close();
+} catch (Exception $e) {
+    error_log("SQLITE3 ERROR: " . $e->getMessage());
+}
+
 if($page == 'schedule') {
     readfile('schedule.html');
-} elseif($page == 'chat') {
+} elseif (!$isAdmin) {
+    readfile('development.html');
+}
+elseif($page == 'chat') {
     readfile('chat.html');
 } elseif($page == 'settings') {
     readfile('settings.html');
